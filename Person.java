d09dca2703fc9ca128977e205a74bc43b9b9e877
shivamBabbar1 <140402031+shivamBabbar1@users.noreply.github.com>
2023-08-12 16:07:44 +0530

    Update Person.java

--- Person.java
+++ Person.java
@@ -1,57 +1,76 @@
-
-public class Person {
-
+ public class Person {
+    //feilds
     private String firstName;
     private String lastName;
     private String address;
     private int mobile;
     private int id;
 
-    Person(){}
+    // constructors
 
-    Person(String name){
+    Person() {
+    }
+
+    Person(String name) {
         firstName = name;
     }
 
-    Person(String firstName, String lastName, String address, int mobile, int id){
-      this.firstName = firstName;
-      this.lastName = lastName;
-      this.address = address;
-      this.mobile = mobile;
-      this.id = id;
+    Person(String firstName, String lastName, String address, int mobile, int id) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.mobile = mobile;
+        this.id = id;
     }
+    // constructors
 
-  public String getFirstName(){
+    // getter methods//
+
+    public String getFirstName() {
         return firstName;
     }
-     public String getLastName(){
+
+    public String getLastName() {
         return lastName;
     }
-     public String getAddress(){
+
+    public String getAddress() {
         return address;
     }
-     public int getMobile(){
+
+    public int getMobile() {
         return mobile;
     }
-     public int getId(){
+
+    public int getId() {
         return id;
     }
 
     // setter methods//
- public String setFirstName(String name){
-        return firstName =name;
+    public String setFirstName(String name) {
+        return firstName = name;
     }
-     public String setLastName(String lname){
+
+    public String setLastName(String lname) {
         return lastName = lname;
     }
-     public String setAddress(String address){
+
+    public String setAddress(String address) {
         return this.address = address;
     }
-     public int setetMobile(int mob){
+
+    public int setetMobile(int mob) {
         return mobile = mob;
     }
-     public int setId(int id){
+
+    public int setId(int id) {
         return this.id = id;
     }
     
+    //override toString
+    @Override
+    public String toString(){
+        return  "Name : "+ firstName +" "+ lastName +"\n" +"Address : "+ address +"\n"+ "mobile : "+ mobile+"\n"+"ID : "+ id;
+    }
+
 }
