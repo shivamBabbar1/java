7483bb84db750fd3df5c7e284356e042e3554132
shivamBabbar1 <140402031+shivamBabbar1@users.noreply.github.com>
2023-08-12 16:06:49 +0530

    Update PersonDemo.java

--- PersonDemo.java
+++ PersonDemo.java
@@ -1,14 +1,10 @@
-package java1;
-
-import Person;
-
 public class PersonDemo {
     public static void main(String args[]){
-        Person p1 = new Person("raam");
-        p1.getFirstName();
-
-        System.out.println("Some changes from remote....... again changed online.");
+        Person p1 = new Person("raam","sharma","phagwara,12 street",83238467,55);
+    //    System.out.println(p1.getFirstName());
+          
+          System.out.println(p1);
     }
     
 }
-    
+
